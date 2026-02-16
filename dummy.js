9273288eb41f249978016512992e06b24053bde3
indigoxela <user@olga.indigofloat.at>
2026-02-16 12:13:43 +0100

    Trigger less errors

--- dummy.js
+++ dummy.js
@@ -4,13 +4,15 @@
  */
 (function ($, Backdrop) {
 
+"use strict";
+
 Backdrop.behaviors.someBehavior = {
   attach: function (context, settings) {
     let unused = 'somevalue';
     let selectProfile = $('#edit-editor-settings-tinymce-settings-profile');
     selectProfile.on('change', function (event) {
-      undefinedFunc();
-    }
+      console.log('huhu');
+    });
   }
 };
 
